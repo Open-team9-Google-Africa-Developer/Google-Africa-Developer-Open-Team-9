277fd33832b3e3a94433133daaa9e6bc366e6b1e
Herman Ceaser <ceaserbanks09@gmail.com>
2022-11-20 11:17:43 +0300

    wip

--- backend/complaint_api/views.py
+++ backend/complaint_api/views.py
@@ -1,13 +1,22 @@
 from rest_framework import generics
 from complaint.models import Complaint
 from .serializers import ComplaintSerializer
+from rest_framework import SAFE_METHODS, BasePermission
 
+class ComplaintUserWritePermission(BasePermission):
+    message = 'Editing Complaint is restricted to author only.'
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.complainant == request.user
 
 class ComplaintList(generics.ListCreateAPIView):
     queryset = Complaint.complaintobjects.all()
     serializer_class = ComplaintSerializer
     
 
-class ComplaintDetail(generics.RetrieveDestroyAPIView):
+class ComplaintDetail(generics.RetrieveUpdateDestroyAPIView, ComplaintUserWritePermission):
+    permission_classes = [ComplaintUserWritePermission]
     queryset = Complaint.objects.all()
     serializer_class = ComplaintSerializer
