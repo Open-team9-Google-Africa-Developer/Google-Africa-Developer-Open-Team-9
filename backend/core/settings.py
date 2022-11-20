8e36aff34af16f618b0a9080ab93996261bc951d
Herman Ceaser <ceaserbanks09@gmail.com>
2022-11-20 11:17:43 +0300

    wip

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,7 +52,11 @@ AUTH_USER_MODEL = "account.CustomUser"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
+    
 }
 
 MIDDLEWARE = [
